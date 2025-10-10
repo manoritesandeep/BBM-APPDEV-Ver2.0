8f41cf337367371b1d1900db244ca9c304a86448
manoritesandeep <manoritesandeep@gmail.com>
2025-10-09 23:01:11 -0700

    updated index.js for products

--- components/Products/index.js
+++ components/Products/index.js
@@ -1,14 +1,14 @@
 /**
  * Products Component Index
- * 
+ *
  * Centralized exports for all product-related components
  */
 
 // Product Card Components
-export { default as HorizontalProductCard } from '../UI/HorizontalProductCard';
-export { default as ProductCard } from './ProductCard';
+export { default as HorizontalProductCard } from "../UI/HorizontalProductCard";
+export { default as ProductCard } from "./ProductCard";
 
 // Product Display Components
-export { default as ProductList } from './ProductList';
-export { default as ProductDetailedSummary } from './ProductDetailedSummary';
-export { default as ProductsOutput } from './ProductsOutput';
+export { default as ProductList } from "./ProductList";
+export { default as ProductDetailedSummary } from "./ProductDetailedSummary";
+export { default as ProductsOutput } from "./ProductsOutput";
